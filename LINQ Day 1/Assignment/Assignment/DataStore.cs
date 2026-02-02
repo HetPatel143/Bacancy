549f79b9895aac3dcdaaf8b9bc80eacb03bbb04f
Het <thehetpatel143@gmail.com>
2026-02-02 18:19:33 +0530

    LINQ Day 1

---
+++ LINQ Day 1/Assignment/Assignment/DataStore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assignment.data;
+
+namespace Assignment
+{
+    public class DataBase
+    {
+        public static List<Employee> employee = new List<Employee>
+        {
+            new Employee{EmpId=1, Name="rahul", Salary=10000,Department="HR",City="Ahmedabad" },
+            new Employee{EmpId=2, Name="rohan", Salary=20000,Department="IT",City="Ahmedabad"  },
+            new Employee{EmpId=3, Name="neil", Salary=30000, Department = "IT", City = "Surat"},
+            new Employee{EmpId=4, Name="nitin", Salary=40000, Department = "Marketing", City = "Rajkot"},
+            new Employee{EmpId=5, Name="mukesh", Salary=50000, Department = "Networking", City = "Mehsana"},
+        };
+        public static List<Student> student = new List<Student>
+    {
+        new Student{RollNo=101, Name="amar", Marks=90},
+        new Student{RollNo=102, Name="akbar", Marks=80},
+        new Student{RollNo=103, Name="anthony", Marks=95},
+        new Student{RollNo=104, Name="karan", Marks=35},
+        new Student{RollNo=105, Name="arjun", Marks=25}
+    };
+        public static List<Order> order = new List<Order>
+    {
+        new Order{OrderId=111,CustomerName="ganga",OrderItems = new List<OrderItem>{
+            new OrderItem{ProductName="laptop",Price=80000},
+            new OrderItem { ProductName = "bag", Price = 1500 }
+            }
+        },
+        new Order{OrderId=222,CustomerName="jamna",OrderItems = new List<OrderItem>{
+            new OrderItem{ProductName="purse",Price=8000},
+            new OrderItem { ProductName = "lipstick", Price = 1700 }
+            }
+        },
+        new Order{OrderId=333,CustomerName="sarasvati",OrderItems = new List<OrderItem>{
+            new OrderItem{ProductName="saree",Price=7000},
+            new OrderItem { ProductName = "eyeliner", Price = 500 }
+            }
+        },
+        new Order{OrderId=444,CustomerName="narendra",OrderItems = new List<OrderItem>{
+            new OrderItem{ProductName="shoes",Price=5000},
+            new OrderItem { ProductName = "watch", Price = 3500 }
+            }
+        },
+        new Order{OrderId=555,CustomerName="amit",OrderItems = new List<OrderItem>{
+            new OrderItem{ProductName="mobile",Price=70000},
+            new OrderItem { ProductName = "cover", Price = 1000 }
+            }
+        }
+    };
+    }
+}
---
+++ LINQ Day 1/Assignment/Assignment/Program.cs
@@ -0,0 +1,259 @@
+﻿using Assignment;
+using System;
+using static Assignment.data;
+using static Assignment.DataBase;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Tasks.Task1();
+        Tasks.Task2();
+        Tasks.Task3();
+        Tasks.Task4();
+        Tasks.Task5();
+        Tasks.Task6();
+        Tasks.Task7();
+        Tasks.Task8();
+        Tasks.Task9();
+        Tasks.Task10();
+    }
+}
+
+public static class Tasks
+{
+    #region Task1
+    public static void Task1()
+    {
+
+        Console.WriteLine("method syntax used");
+        Console.WriteLine("-----------------------TASK 1 START-----------------------");
+        Console.WriteLine();
+        Console.WriteLine("employee with salary >25000");
+        Console.WriteLine();
+        var HigherSalary = employee.Where(s => s.Salary > 25000).Select(s => $"{s.EmpId}-{s.Name} {s.Salary}");
+        //Console.WriteLine(string.Join(",",HighestSalary));
+        foreach (var i in HigherSalary)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 1 END");
+        Console.WriteLine();
+
+        //THEORY: HERE I USED WHERE TO FILTER THE EMPLOYEES WITH SALARY ABOVE 25000
+        //SELECT IS USED HERE TO RETRIVE DATA IN STRING FORMAT
+
+    }
+    #endregion
+
+    #region Task2
+    public static void Task2()
+    {
+
+        Console.WriteLine("-----------------------TASK 2 START-----------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------employees in IT Department-----------------------");
+        Console.WriteLine();
+
+        var DepartmentIT = employee.Where(s => s.Department == "IT").Select(s => $"{s.Name} {s.Salary}");
+        foreach (var i in DepartmentIT)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 2 END");
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //THEORY: WHERE FILTERS EMPLOYEE BASED ON DEPARTMENT AND SELECT GIVES DATA OF EMPLOYEE
+
+    }
+    #endregion
+
+    #region Task3
+    public static void Task3()
+    {
+        Console.WriteLine("-----------------------TASK 3 START-----------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------employees in IT Department-----------------------");
+        Console.WriteLine();
+        var OrderSalary = employee.Where(s => s.Salary > 30000).OrderBy(s => s.Salary).Select(s => $"{s.EmpId}-{s.Name} {s.Salary}");
+        foreach (var i in OrderSalary)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 3 END");
+        Console.WriteLine();
+
+        //THEORY: WHERE FILTERS THE EMPLOYEE SALARY ABOVE 30000 
+        //ORDERBY IS USED TO SORT BASED ON SALARY IN ASCENDING ORDER
+        //SELECT IS USED HERE TO RETRIVE DATA IN STRING FORMAT
+
+    }
+    #endregion
+
+    #region Task4
+    public static void Task4()
+    {
+        Console.WriteLine("-----------------------TASK 4 START-----------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------employees sortedby department and then name-----------------------");
+        Console.WriteLine();
+        var SortName = employee.OrderBy(s => s.Department).ThenBy(s => s.Name).Select(s => $"{s.Department}-{s.Name}");
+        foreach (var i in SortName)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 4 END");
+        Console.WriteLine();
+
+        //HERE ORDERBY FIRST SORTS BASED ON DEPARTMENT AND THENBY THEN SORTS BASED ON THE NAME
+        //SELECT IS USED HERE TO RETRIVE DATA IN STRING FORMAT
+
+
+    }
+    #endregion
+
+    #region Task5
+    public static void Task5()
+    {
+
+
+        var Result = student.Select(s => new { s.Name, s.Marks, Result = s.Marks >= 40 ? "Pass" : "Fail" });
+        Console.WriteLine("----------------------------TASK 5----------------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------student result pass or fail-----------------------");
+        Console.WriteLine();
+        foreach (var i in Result)
+        {
+            Console.WriteLine($"{i.Name} - {i.Marks} - {i.Result}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 5 END");
+        Console.WriteLine();
+
+        //SELECT IS USED HERE TO RETRIVE STUDENT DATA WITH ANONYMOUS TYPE 
+        //TERNARY OPERATOR USED TO COMPUTE PASS OR FAIL
+
+    }
+    #endregion
+
+    #region Task6
+    public static void Task6()
+    {
+
+
+        var AnonymousFun = student.Select(s => new { s.Name, s.Marks, s.RollNo });
+        Console.WriteLine("----------------------------TASK 6----------------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------anonymous type-----------------------");
+        Console.WriteLine();
+        foreach (var i in AnonymousFun)
+        {
+            Console.WriteLine($"{i.Name} - {i.Marks} - {i.RollNo}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 6 END");
+        Console.WriteLine();
+
+        //SELECT IS USED HERE TO RETRIVE STUDENT DATA WITH ANONYMOUS TYPE 
+
+
+    }
+    #endregion
+
+    #region Task7
+    public static void Task7()
+    {
+
+        var NameProduct = order.SelectMany(s => s.OrderItems).Select(s => s.ProductName);
+
+        Console.WriteLine("----------------------------TASK 7----------------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------products name from orders-----------------------");
+        Console.WriteLine(); foreach (var i in NameProduct)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 7 END");
+        Console.WriteLine();
+
+        //HERE SELECTMANY IS USED TO FLATTEN THE NESTED ORDERITEMS AND SELECT RETRIVES PRODUCT NAME
+
+    }
+    #endregion
+
+    #region Task8
+    public static void Task8()
+    {
+        var CustomerProduct = order.SelectMany(o => o.OrderItems, (o, item) => new { o.CustomerName, item.ProductName });
+
+        Console.WriteLine("----------------------------TASK 8----------------------------");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------customer name and product name-----------------------");
+        Console.WriteLine();
+        foreach (var i in CustomerProduct)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 8 END");
+        Console.WriteLine();
+
+        //HERE SELECTMANY IS USED WITH ANONYMOUS TYPE AND RETRIVES CUSTOMER NAME AND PRODUCT NAME
+
+    }
+    #endregion
+
+    #region Task9
+    public static void Task9()
+    {
+        Console.WriteLine("-----------------------TASK 9 START-----------------------");
+        Console.WriteLine("-----------------------employees names in new collection-----------------------");
+        Console.WriteLine();
+        List<string> EmployeeNames = employee.Select(s => $"{s.Name}").ToList();
+
+        foreach (var i in EmployeeNames)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+        Console.WriteLine("TASK 9 END");
+        Console.WriteLine();
+
+        //HERE I EXTRACTED EMPLOYEE NAMES AND USING .TOLIST() STORED IN A COLLECTION
+
+    }
+    #endregion
+
+    #region Task10
+    public static void Task10()
+    {
+        var QuerySyntax = from e in employee
+                          where e.Salary > 25000
+                          select e;
+        var MethodSyntax = employee.Where(s => s.Salary > 25000).Select(s => $"{s.EmpId}-{s.Name} {s.Salary}");
+
+        Console.WriteLine("----------------------------TASK 10----------------------------");
+
+        Console.WriteLine("----------------------------Query syntax----------------------------");
+        foreach (var i in QuerySyntax)
+        {
+            Console.WriteLine($"{i.EmpId} - {i.Name} - {i.Salary}");
+        }
+        Console.WriteLine("----------------------------Method syntax----------------------------");
+        foreach (var i in MethodSyntax)
+        {
+            Console.WriteLine(i);
+        }
+
+        // QUERY SYNTAX IS LIKE SQL USES KEYWORDS LIKE FROM , WHERE, SELECT
+        //QUERY SYNTAX IS INTERNALLY CONVERTED INTO METHOD SYNTAX
+        // METHOD SYNTAX USES EXTENSION METHODS WHERE(),SELECT()
+
+    }
+    #endregion
+}
